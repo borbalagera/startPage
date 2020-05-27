d4434d77a50e118b9f3857c895d2ed067401ef1a
Gera Borbála <37974097+borbalagera@users.noreply.github.com>
2020-05-27 16:13:33 +0200

    Added color change & onLoad color

--- javascript.js
+++ javascript.js
@@ -1,15 +1,25 @@
 window.onload = () => {
-  document.body.style.backgroundColor = "#52809c";
-  //   $.getJSON(
-  //     "http://api.openweathermap.org/data/2.5/weather?q=Budapest&units=metric&units=metric&appid=4b52df0c72d732b4a364560667ecbafb",
-  //     function (data) {
-  //       console.log(data);
-  //       var temp = " " + data.main.temp + " ℃";
-  //       var realFeel = " " + data.main.feels_like + " ℃";
-  //       $(".temp").append(temp);
-  //       $(".realFeel").append(realFeel);
-  //     }
-  //   );
+  const colors = ["#f8f8a1", "#f9c7be", "#94bca4", "#52809c", "#6c6c74"];
+  let rand = Math.floor(Math.random() * Math.floor(5));
+  document.body.style.backgroundColor = colors[rand];
+
+  let beginning = "http://api.openweathermap.org/data/2.5/weather?q=";
+  let city = "";
+  let unit = "&units=metric";
+  let apikey = "&appid=4b52df0c72d732b4a364560667ecbafb";
+  city = document.getElementById("location").value;
+  let url = beginning + city + unit + apikey;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      $(".temp").empty();
+      $(".realFeel").empty();
+      var temp = " " + data.main.temp + " ℃";
+      var realFeel = " " + data.main.feels_like + " ℃";
+      $(".temp").append(temp);
+      $(".realFeel").append(realFeel);
+    });
 };
 
 function showTime() {
@@ -82,3 +92,23 @@ function locationChange() {
 }
 
 locationChange();
+
+function changeYellow() {
+  document.body.style.backgroundColor = "#f8f8a1";
+}
+
+function changePink() {
+  document.body.style.backgroundColor = "#f9c7be";
+}
+
+function changeGreen() {
+  document.body.style.backgroundColor = "#94bca4";
+}
+
+function changeBlue() {
+  document.body.style.backgroundColor = "#52809c";
+}
+
+function changeGray() {
+  document.body.style.backgroundColor = "#6c6c74";
+}
